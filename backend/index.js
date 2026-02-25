cddba56baff6dcfa1f1490cf3ecac9038bbef1fd
KRRISH AGARWAL <krrishapj@gmail.com>
2026-02-25 19:43:27 +0530

    Socket BE logic

--- backend/index.js
+++ backend/index.js
@@ -13,3 +13,32 @@ const io = new Server({
     credentials: true,
   },
 });
+
+io.on("connection", (socket) => {
+  console.log("User Connected:", socket.id);
+
+  // Join a specific room based on Session ID
+  socket.on("join-session", (sessionId) => {
+    console.log("Session joined", sessionId);
+    socket.join(sessionId);
+  });
+
+  // Listen for movement from Tracker
+  socket.on("update-map", (data) => {
+    // data: { sessionId, lat, lng, zoom }
+    socket.to(data.sessionId).emit("Map-moved", {
+      lat: data.lat,
+      lng: data.lng,
+      zoom: data.zoom,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
